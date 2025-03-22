b9eeaaacf9a07875de56d7092b159fd381e4b023
Dylan Powers <dylanpowers125@gmail.com>
2025-03-22 13:48:48 -0500

    Got the visualization box

---
+++ src/VisualizationArea.tsx
@@ -0,0 +1,9 @@
+function VisualizationArea() {
+  return (
+    <div id="visualization-area-border">
+      <textarea style={{ resize: 'none' }} value={''} readOnly={true} />
+    </div>
+  )
+}
+
+export default VisualizationArea
